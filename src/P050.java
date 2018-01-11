809b14708402ce7454d91a4e4eb6fd9bf8b699c0
Mike Chen <stupidchen@foxmail.com>
2018-01-11 22:44:26 +0800

    Finished: P050

---
+++ src/P050.java
@@ -0,0 +1,19 @@
+/**
+ * Created by mike on 1/11/18.
+ */
+public class P050 {
+    private double pow(double x, long n) {
+        if (n == 0) return 1.0d;
+        double t = pow(x, n >> 1);
+        t *= t;
+        if ((n & 1) == 1) t *= x;
+        return t;
+    }
+    public double myPow(double x, int n) {
+        if (x == 0) return 0;
+        if (n < 0)
+            return 1 / pow(x, 0L - (long) n);
+        else
+            return pow(x, n);
+    }
+}
