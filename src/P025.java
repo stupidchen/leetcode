893a404c2c28ac989a5bc797b8667306c93133eb
Mike Chen <stupidchen@foxmail.com>
2018-01-22 22:29:05 +0800

    Finished P025.

---
+++ src/P025.java
@@ -0,0 +1,46 @@
+/**
+ * Created by mike on 1/22/18.
+ */
+public class P025 {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null) return null;
+
+        ListNode ret = head;
+        ListNode last = null;
+        while (head != null) {
+            int t = 0;
+            ListNode now = head;
+            while (now != null && t < k) {
+                t++;
+                now = now.next;
+            }
+            if (t == k) {
+                now = head;
+                t = 1;
+                ListNode tmp, before = null;
+                while (t < k) {
+                    tmp = now.next;
+                    now.next = before;
+                    before = now;
+                    now = tmp;
+                    t++;
+                }
+                if (last == null)
+                    ret = now;
+                else
+                    last.next = now;
+                last = head;
+                if (now != null) {
+                    head = now.next;
+                    now.next = before;
+                }
+            }
+            else {
+                if (last != null) last.next = head;
+                break;
+            }
+        }
+
+        return ret;
+    }
+}
