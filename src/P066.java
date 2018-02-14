a7fdf861cc85c3b11ee2bdc957b6e7b52d9efaff
Mike Chen <stupidchen@foxmail.com>
2018-02-14 13:24:43 +0800

    Fixed P066.

--- src/P066.java
+++ src/P066.java
@@ -5,14 +5,20 @@ import java.util.Arrays;
  */
 public class P066 {
     public int[] plusOne(int[] digits) {
-        int[] ret = Arrays.copyOf(digits, digits.length + 1);
+        int i = digits.length - 1;
+        digits[i] = digits[i] + 1;
+        while (i > 0 && digits[i] >= 10) {
+            digits[i] -= 10;
+            digits[i - 1] += 1;
+            i--;
+        }
 
-        ret[0] = ret[0] + 1;
-        int i = 0;
-        while (ret[i] >= 10) {
-            ret[i] -= 10;
-            ret[i + 1] += 1;
-            i++;
+        int[] ret = digits;
+        if (digits[i] >= 10) {
+            ret = new int[digits.length + 1];
+            ret[0] = 1;
+            digits[i] -= 10;
+            for (i = 0; i < digits.length; i++) ret[i + 1] = digits[i];
         }
 
         return ret;
