058bb4493110e22a3ff40e70fdd04c3605c8a87d
Mike Chen <stupidchen@foxmail.com>
2018-02-18 00:06:29 +0800

    Finished P101.

--- src/P101.java
+++ src/P101.java
@@ -2,7 +2,13 @@
  * Created by mike on 2/17/18.
  */
 public class P101 {
+    public boolean isSymmetricRecursive(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) return true;
+        if (root1 == null || root2 == null) return false;
+        if (root1.val != root2.val) return false;
+        return isSymmetricRecursive(root1.left, root2.right) && isSymmetricRecursive(root1.right, root2.left);
+    }
     public boolean isSymmetric(TreeNode root) {
-
+        return isSymmetricRecursive(root, root);
     }
 }
