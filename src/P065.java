e34da182f9177fd5875d3650f9a944f2c3318af3
Mike Chen <stupidchen@foxmail.com>
2018-02-23 17:26:40 +0800

    Finished P065.

---
+++ src/P065.java
@@ -0,0 +1,18 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by mike on 2/23/18.
+ */
+public class P065 {
+    public boolean isNumber(String s) {
+        if (s == null) return false;
+        Pattern number = Pattern.compile("\\s*(\\+|-)?(\\d+(\\.\\d*)?|\\.\\d+)(e(\\+|-)?\\d+)?\\s*$");
+        return number.matcher(s).matches();
+    }
+
+    public static void main(String[] args) {
+        P065 p = new P065();
+        System.out.println(p.isNumber("3e1.6"));
+    }
+}
