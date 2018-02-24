c64c53d180f62237f9273d1a985cd80f0a1affd6
Mike Chen <stupidchen@foxmail.com>
2018-02-25 00:36:32 +0800

    Finished P075.

---
+++ src/P075.java
@@ -0,0 +1,13 @@
+/**
+ * Created by mike on 2/24/18.
+ */
+public class P075 {
+    public void sortColors(int[] nums) {
+        int[] cn = new int[3];
+        for (int i = 0; i < nums.length; i++) cn[nums[i]]++;
+
+        int t = 0;
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < cn[i]; j++) nums[t++] = i;
+    }
+}
