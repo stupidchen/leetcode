3b606ce3df7efa4778870e1df37c9b6e7cd36529
Mike Chen <stupidchen@foxmail.com>
2018-02-28 21:51:31 +0800

    Finished P061.

---
+++ src/P061.java
@@ -0,0 +1,29 @@
+/**
+ * Created by mike on 2/28/18.
+ */
+public class P061 {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null) return null;
+
+        int n = 1;
+        ListNode tmp = head;
+        while (tmp.next != null) {
+            n++;
+            tmp = tmp.next;
+        }
+
+        if (n == 1) return head;
+
+        tmp.next = head;
+        tmp = head;
+        int m = 0;
+        k = k % n;
+        while (m != n - k - 1) {
+            m++;
+            tmp = tmp.next;
+        }
+        head = tmp.next;
+        tmp.next = null;
+        return head;
+    }
+}
