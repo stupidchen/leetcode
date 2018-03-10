1a71a89da6264920005e2d690bf0328491a38494
Mike Chen <stupidchen@foxmail.com>
2018-03-10 14:45:21 +0800

    Finished P086.

---
+++ src/P086.java
@@ -0,0 +1,33 @@
+/**
+ * Created by mike on 3/10/18.
+ */
+public class P086 {
+    public ListNode partition(ListNode head, int x) {
+        ListNode cacheLast = null, cacheHead = null;
+        ListNode last = null;
+        ListNode tmp = head;
+        while (tmp != null) {
+            if (tmp.val >= x) {
+                if (cacheHead == null)
+                    cacheHead = tmp;
+                else
+                    cacheLast.next = tmp;
+                cacheLast = tmp;
+            }
+            else {
+                if (last == null)
+                    head = tmp;
+                else
+                    last.next = tmp;
+                last = tmp;
+            }
+            tmp = tmp.next;
+        }
+        if (cacheLast != null)
+            cacheLast.next = null;
+        if (last != null)
+            last.next = cacheHead;
+        else head = cacheHead;
+        return head;
+    }
+}
