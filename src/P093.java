dce5631d7d26c037c74095fc4d7d5baac78b6373
Mike Chen <stupidchen@foxmail.com>
2018-03-20 23:54:02 +0800

    Added P093.

---
+++ src/P093.java
@@ -0,0 +1,36 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by mike on 3/20/18.
+ */
+public class P093 {
+    private HashSet<String> solutionsMap;
+
+    private void solve(String str, String solution, int cur, int adn) {
+        if (cur >= str.length() && adn == 4) {
+            solutionsMap.add(solution);
+            return;
+        }
+        if (cur >= str.length()) return;
+        if (adn >= 4) return;
+
+        for (int i = 1; i < 4; i++) {
+            if (cur + i > str.length()) break;
+            String ts = str.substring(cur, cur + i);
+            int t = Integer.valueOf(ts);
+            ts = String.valueOf(t);
+            if (adn != 0) ts = '.' + ts;
+            if (t <= 255) solve(str, solution + ts, cur + i, adn + 1);
+        }
+    }
+
+    public List<String> restoreIpAddresses(String s) {
+        solutionsMap = new HashSet<>();
+        solve(s, "", 0, 0);
+        LinkedList<String> solutions = new LinkedList<>();
+        solutions.addAll(solutionsMap);
+        return solutions;
+    }
+}
