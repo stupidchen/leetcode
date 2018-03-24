7d0ecb56ac8388b06bf5801fcf58f32739e05a20
Mike Chen <stupidchen@foxmail.com>
2018-03-24 18:48:41 +0800

    Finished P121.

--- src/P121.java
+++ src/P121.java
@@ -4,6 +4,17 @@
 public class P121 {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        return 0;
+        int ret = 0, c = 0;
+        for (int i = 1; i < n; i++) {
+            c = Math.max(0, prices[i] - prices[i - 1] + c);
+            ret = Math.max(c, ret);
+        }
+
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        P121 p = new P121();
+        System.out.println(p.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
     }
 }
