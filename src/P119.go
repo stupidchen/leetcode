3fe9ff167721d202562d783c5837ffa9c994bb80
Mike Chen <stupidchen@foxmail.com>
2018-04-09 23:37:29 +0800

    Finished P119.

---
+++ src/P119.go
@@ -0,0 +1,24 @@
+package leetcode
+
+
+func getRow(rowIndex int) []int {
+	var f [2][1000000]int
+	f[0][0] = 1
+	f[1][0] = 1
+	f[1][1] = 1
+	for i := 2; i <= rowIndex; i++ {
+		for j := 0; j <= i; j++ {
+			var l = 0
+			if j > 0 {
+				l = f[1 - (i & 1)][j - 1]
+			}
+			var r = f[1 - (i & 1)][j]
+			f[i & 1][j] = l + r
+		}
+	}
+	return f[rowIndex & 1][0:rowIndex]
+}
+
+func main() {
+	getRow(0)
+}
