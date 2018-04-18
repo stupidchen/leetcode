eb2ddaf9cfc69de0bc088081c8deca74ea7cca94
Mike Chen <stupidchen@foxmail.com>
2018-04-19 02:50:12 +0800

    Finished P106.

---
+++ src/P106.go
@@ -0,0 +1,20 @@
+package leetcode
+
+func buildTree(inorder []int, postorder []int) *TreeNode {
+	if len(postorder) == 0 {
+		return nil
+	}
+	if len(postorder) == 1 {
+		return &TreeNode{Val: postorder[0]}
+	}
+	var n = len(postorder)
+	var c = TreeNode{Val: postorder[n - 1]}
+	for i := 0; i < len(inorder); i++ {
+		if inorder[i] == postorder[n - 1] {
+			c.Left = buildTree(inorder[: i], postorder[0: i])
+			c.Right = buildTree(inorder[i + 1: ], postorder[i: n - 1])
+			break
+		}
+	}
+	return &c
+}
