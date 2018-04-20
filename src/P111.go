9a89ca1fdf21e659844dae6f5450d2487fcf4f02
Mike Chen <stupidchen@foxmail.com>
2018-04-20 23:52:30 +0800

    Finished P111.

---
+++ src/P111.go
@@ -0,0 +1,24 @@
+package leetcode
+
+func min(x int, y int) int {
+	if x > y {
+		return y
+	}
+	return x
+}
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	} else {
+		var l = minDepth(root.Left)
+		var r = minDepth(root.Right)
+		if l == 0 {
+			return r + 1
+		}
+		if r == 0 {
+			return l + 1
+		}
+		return min(l, r) + 1
+	}
+}
