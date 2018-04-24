7434b2e7e2bfdea662244609948ef8a4ff27cd01
Mike Chen <stupidchen@foxmail.com>
2018-04-25 01:02:22 +0800

    Finished P144.

--- src/P144.go
+++ src/P144.go
@@ -1,5 +1,20 @@
 package leetcode
 
-func preorderTraversal(root *TreeNode) []int {
+func solvePreorderTraversal(current *TreeNode, preorder []int, pos *int) {
+	if current == nil {
+		return
+	}
 
+	preorder[*pos] = current.Val
+	*pos++
+	solvePreorderTraversal(current.Left, preorder, pos)
+	solvePreorderTraversal(current.Right, preorder, pos)
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	var ret [10000]int
+	var n = 0
+
+	solvePreorderTraversal(root, ret[: ], &n)
+	return ret[: n]
 }
