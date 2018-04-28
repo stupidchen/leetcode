18757683c7064e9f08ec6cc02cf5435504621c8b
Mike Chen <stupidchen@foxmail.com>
2018-04-29 03:35:41 +0800

    Finished P129.

--- src/P129.go
+++ src/P129.go
@@ -1,1 +1,19 @@
 package leetcode
+
+func countSum(cur *TreeNode, sum *int, num int) {
+	if cur == nil {
+		return
+	}
+	var tmp = num * 10 + cur.Val
+	if cur.Left == nil && cur.Right == nil {
+		*sum = *sum + tmp
+	}
+	countSum(cur.Left, sum, tmp)
+	countSum(cur.Right, sum, tmp)
+}
+
+func sumNumbers(root *TreeNode) int {
+	var ret = 0
+	countSum(root, &ret, 0)
+	return ret
+}
