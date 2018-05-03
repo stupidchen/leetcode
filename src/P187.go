e3dcd13f14b15c09af0ffa17204caedf9e1f6dd4
Mike Chen <stupidchen@foxmail.com>
2018-05-03 22:56:46 +0800

    Finished P187.

---
+++ src/P187.go
@@ -0,0 +1,20 @@
+package leetcode
+
+import "strings"
+
+func findRepeatedDnaSequences(s string) []string {
+	var ans [1000]string
+	var f = make(map[string]bool)
+	var m = 0
+
+	for i := 0; i < len(s) - 10; i++ {
+		var t = s[i: i + 10]
+		if strings.Contains(s[i + 1: ], t) && !f[t] {
+			ans[m] = t
+			f[t] = true
+			m++
+		}
+	}
+
+	return ans[0: m]
+}
