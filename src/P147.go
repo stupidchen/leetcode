ce3b09fecc42af043443d47971231826605128bc
Mike Chen <stupidchen@foxmail.com>
2018-05-05 23:34:34 +0800

    Finished P147.

---
+++ src/P147.go
@@ -0,0 +1,25 @@
+package leetcode
+
+func insertionSortList(head *ListNode) *ListNode {
+	var new_head = &ListNode{
+		Next: nil,
+		Val: int(0x7FFFFFFF + 1),
+	}
+
+	var tmp *ListNode
+	for i := head; i != nil; i = tmp {
+		tmp = i.Next
+		var tmp2 *ListNode
+		for j := new_head; j != nil; j = tmp2 {
+			tmp2 = j.Next
+			if j.Next == nil || j.Next.Val > i.Val {
+				var next = j.Next
+				j.Next = i
+				i.Next = next
+				break
+			}
+		}
+	}
+
+	return new_head.Next
+}
