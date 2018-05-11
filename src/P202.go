a04ae92fbda52ac13b54083f2dbfbb7a861be82b
Mike Chen <stupidchen@foxmail.com>
2018-05-12 00:27:44 +0800

    Finished P202.

--- src/P202.go
+++ src/P202.go
@@ -1,1 +1,22 @@
 package leetcode
+
+func nextSquareSum(n int) int {
+	var ret = 0
+	for ;n > 0; n /= 10 {
+		var t = n % 10
+		ret += t * t
+	}
+	return ret
+}
+
+func isHappy(n int) bool {
+	var f [1000]bool
+	n = nextSquareSum(n)
+	for ; n != 1; n = nextSquareSum(n) {
+		if f[n] {
+			return false
+		}
+		f[n] = true
+	}
+	return true
+}
