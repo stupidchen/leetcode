ccf226678dda8b5c07738afa5a9595f88a74b2dc
Mike Chen <stupidchen@foxmail.com>
2018-05-31 23:06:16 +0800

    Finished P204.

--- src/P204.go
+++ src/P204.go
@@ -1,5 +1,23 @@
 package leetcode
 
+var prime [10000000]bool
 func countPrimes(n int) int {
+	prime[0] = false
+	prime[1] = false
+	prime[2] = true
+	for i := 2; i < n; i++ {
+		prime[i] = true
+	}
 
+	var ans = 0
+	for i := 2; i < n; i++ {
+		if prime[i] {
+			ans++
+			for j := i * i; j < n; j += i {
+				prime[j] = false
+			}
+		}
+	}
+
+	return ans
 }
