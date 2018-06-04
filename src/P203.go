514da8c4dfb5bd52175a35480ccbaebaf16dd24d
Mike Chen <stupidchen@foxmail.com>
2018-06-04 23:49:54 +0800

    Finished P203.

---
+++ src/P203.go
@@ -0,0 +1,18 @@
+package leetcode
+
+func removeElements(head *ListNode, val int) *ListNode {
+	var first = ListNode{
+		Next:head,
+	}
+
+	var last = &first
+	for tmp := head; tmp != nil; tmp = tmp.Next {
+		if tmp.Val == val {
+			last.Next = tmp.Next
+		} else {
+			last = tmp
+		}
+	}
+
+	return first.Next
+}
