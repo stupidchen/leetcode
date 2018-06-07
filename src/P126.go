097729affca91af6805a6f7ae5d6fdb43ece8264
Mike Chen <stupidchen@foxmail.com>
2018-06-07 21:47:23 +0800

    Updated README.

--- src/P126.go
+++ src/P126.go
@@ -1,1 +1,28 @@
 package leetcode
+
+const maxn = 1000
+var wordMap [maxn][maxn]int
+var n int
+
+func wordDiffNum(x string, y string) int {
+	if len(x) != len(y) {
+		return -1
+	}
+
+	var ret = 0
+	for i := 0; i < len(x); i++ {
+		if x[i] != y[i] {
+			ret++
+		}
+	}
+
+	return ret
+}
+
+func findLadders(beginWord string, endWord string, wordList []string) [][]string {
+	n = len(wordList)
+	var ret [][]string
+	if n == 0 {
+		return ret
+	}
+}
--- src/P201.go
+++ src/P201.go
@@ -1,1 +1,21 @@
 package leetcode
+
+func countBit(m int, n uint) int {
+	if 1 << n <= m {
+		return m - (1 << n)
+	}
+	return 0
+}
+
+func rangeBitwiseAnd(m int, n int) int {
+	var i uint
+	var ret = 0
+	for i = 0; (1 << i) <= n; i++ {
+		t := countBit(n, i) - countBit(m, i)
+		if (t & 1) == 1 {
+			ret = ret | (1 << i)
+		}
+	}
+
+	return ret
+}
