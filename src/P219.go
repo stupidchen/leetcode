913f8f54c0ef617c2829288ce92233325697da8c
Mike Chen <stupidchen@foxmail.com>
2018-06-13 22:31:42 +0800

    Finished P219.

--- src/P219.go
+++ src/P219.go
@@ -1,1 +1,13 @@
 package leetcode
+
+func containsNearbyDuplicate(nums []int, k int) bool {
+	n := len(nums)
+	for i := 1; i < n; i++ {
+		for j := i - 1; j >= i - k && j >= 0; j-- {
+			if nums[i] == nums[j] {
+				return true
+			}
+		}
+	}
+	return false
+}
