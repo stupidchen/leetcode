e7339a6f83c73d4a54d1dfb100136e76b8e3cc91
Mike Chen <stupidchen@foxmail.com>
2018-06-18 21:22:50 +0800

    Finished P128.

--- src/P128.go
+++ src/P128.go
@@ -1,5 +1,29 @@
 package leetcode
 
-func longestConsecutive(nums []int) int {
+import "fmt"
 
+func longestConsecutive(nums []int) int {
+	f := make(map[int]int)
+	ret := 0
+	for i := 0; i < len(nums); i++ {
+		f[nums[i]] = 1
+	}
+	for i := 0; i < len(nums); i++ {
+		if _, e := f[nums[i] - 1]; !e {
+			var t int
+			for t = 0; ; t++ {
+				if _, e := f[nums[i] + t]; !e {
+					break
+				}
+			}
+			if t > ret {
+				ret = t
+			}
+		}
+	}
+	return ret
+}
+
+func main() {
+	fmt.Println(longestConsecutive([]int {100,4,200,1,3,2,201,101,203,99,98,204,202}))
 }
