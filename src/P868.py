b1f3a47367829880faf211108f85870a0db1672c
Mike Chen <stupidchen@foxmail.com>
2018-07-15 11:42:13 +0800

    Finished P868.

--- src/P868.py
+++ src/P868.py
@@ -1,3 +1,18 @@
-class Solution(object):
-    def transpose(self, A):
-        return zip(*A)
+class Solution:
+    def binaryGap(self, N):
+        l = -1
+        i = 0
+        ret = 0
+        while N > 0:
+            t = N & 1
+            if t == 1:
+                if l != -1:
+                    if i - l > ret:
+                        ret = i - l
+                l = i
+            i += 1
+            N = N >> 1
+        return ret
+
+if __name__ == '__main__':
+    print(Solution().binaryGap(128838383))
