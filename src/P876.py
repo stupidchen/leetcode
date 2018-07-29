3a7cf7b5a8209c058365ff440c6a7c80a5158418
Mike Chen <stupidchen@foxmail.com>
2018-07-29 16:46:37 +0800

    Finished P876.

---
+++ src/P876.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def middleNode(self, head):
+        len = 0
+        tmp = head
+        while tmp is not None:
+            len += 1
+            tmp = tmp.next
+
+        len = len >> 1
+        for i in range(len):
+            head = head.next
+        return head
