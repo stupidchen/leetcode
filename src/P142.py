e6dcf966bd1d094a593789aa9d227c89750522f8
Mike Chen <stupidchen@foxmail.com>
2018-08-12 00:17:38 +0800

    Finished P142.

--- src/P142.py
+++ src/P142.py
@@ -4,5 +4,25 @@ class ListNode(object):
         self.val = x
         self.next = None
 
+
 class Solution(object):
     def detectCycle(self, head):
+        if head is None:
+            return
+        x = head
+        y = head
+        c = False
+        while x.next is not None and y.next is not None and y.next.next is not None:
+            x = x.next
+            y = y.next.next
+            if x == y:
+                c = True
+                break
+
+        if not c:
+            return
+        x = head
+        while x != y:
+            x = x.next
+            y = y.next
+        return x
