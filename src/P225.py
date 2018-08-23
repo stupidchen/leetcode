c150ddf59cf203ed2c816ef347d2901ee95bc0a9
Mike Chen <stupidchen@foxmail.com>
2018-08-23 23:49:40 +0800

    Finished P225.

---
+++ src/P225.py
@@ -0,0 +1,46 @@
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x):
+        """
+        Push element x onto stack.
+        :type x: int
+        :rtype: void
+        """
+        self.stack.append(x)
+
+    def pop(self):
+        """
+        Removes the element on top of the stack and returns that element.
+        :rtype: int
+        """
+        ret = self.stack[-1]
+        self.stack = self.stack[:-1]
+        return ret
+
+    def top(self):
+        """
+        Get the top element.
+        :rtype: int
+        """
+        ret = self.stack[-1]
+        return ret
+
+    def empty(self):
+        """
+        Returns whether the stack is empty.
+        :rtype: bool
+        """
+        return len(self.stack) == 0
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
