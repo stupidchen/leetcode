c001ec170d63863e22103c7d9999a7ea25c7b407
Mike Chen <stupidchen@foxmail.com>
2018-09-05 23:24:38 +0800

    Finished P146.

--- src/P146.py
+++ src/P146.py
@@ -1,15 +1,47 @@
-class LRUCache:
+class ListNode:
+    def __init__(self, key, val, next=None, last=None):
+        self.key = key
+        self.val = val
+        self.next = next
+        self.last = last
 
+
+class LRUCache:
     def __init__(self, capacity):
         """
         :type capacity: int
         """
+        self.map = {}
+        self.capacity = capacity
+        self.head = ListNode(-1, -1)
+        self.tail = self.head
+
+    def _set_next_node(self, node, target):
+        if target.next == node:
+            return
+        tn = target.next
+        if tn:
+            tn.last = node
+        target.next = node
+        nn = node.next
+        if nn:
+            nn.last = node.last
+        else:
+            self.tail = node.last
+        node.last.next = nn
+        node.last = target
+        node.next = tn
 
     def get(self, key):
         """
         :type key: int
         :rtype: int
         """
+        if key in self.map:
+            self._set_next_node(self.map[key], self.head)
+            return self.map[key].val
+        else:
+            return -1
 
     def put(self, key, value):
         """
@@ -17,8 +49,33 @@ class LRUCache:
         :type value: int
         :rtype: void
         """
+        if key in self.map:
+            self._set_next_node(self.map[key], self.head)
+            self.map[key].val = value
+        else:
+            if self.capacity == 0:
+                self.capacity = 1
+                self.tail.last.next = None
+                del self.map[self.tail.key]
+                tmp = self.tail
+                self.tail = self.tail.last
+                del tmp
+            node = ListNode(key, value, last=self.tail)
+            self.map[key] = node
+            self.tail.next = node
+            self.tail = node
+            self._set_next_node(node, self.head)
+            self.capacity -= 1
 
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+
+if __name__ == '__main__':
+    cache = LRUCache(2)
+    cache.put(1, 1)
+    cache.put(2, 2)
+    print(cache.get(1))
+    cache.put(3, 3)
+    print(cache.get(2))
+    cache.put(4, 4)
+    print(cache.get(1))
+    print(cache.get(3))
+    print(cache.get(4))
