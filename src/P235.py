0eb39147635dbc59a8cccddd45f7ee67be83e1a2
Mike Chen <stupidchen@foxmail.com>
2018-09-13 23:52:08 +0800

    Added P235.

---
+++ src/P235.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        if (root.val - p.val) * (root.val - q.val) < 0:
+            return root
+        if root.val - p.val < 0:
+            return self.lowestCommonAncestor(root.right, p, q)
+        return self.lowestCommonAncestor(root.left, p, q)
