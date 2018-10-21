61ead99ae0f45b2f3cc1c0cf1d98f6133ded2a45
Mike Chen <stupidchen@foxmail.com>
2018-10-21 23:58:31 +0800

    Finished P926.

---
+++ src/P926.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minFlipsMonoIncr(self, S):
+        """
+        :type S: str
+        :rtype: int
+        """
+        n = len(S)
+        f, g = [0] * (n + 1), [0] * (n + 1)
+        for i in range(n):
+            f[i + 1] = f[i] + (1 if S[i] == '1' else 0)
+        for i in reversed(range(n)):
+            g[i] = g[i + 1] + (1 if S[i] == '0' else 0)
+        ret = f[0] + g[0]
+        for i in range(n):
+            ret = min(ret, f[i] + g[i])
+            if g[i] == n - i:
+                ret = min(ret, f[i + 1])
+            if f[i] == i:
+                ret = min(ret, g[i])
+        return ret
+
+if __name__ == '__main__':
+    print(Solution().minFlipsMonoIncr('00011000'))
