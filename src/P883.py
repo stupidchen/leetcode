8aabd0fbcd2b1b6e774d160cab8d4c9c844e6795
Mike Chen <stupidchen@foxmail.com>
2018-11-08 00:01:27 +0800

    Finished P883.

--- src/P883.py
+++ src/P883.py
@@ -11,11 +11,11 @@ class Solution:
                 if grid[i][j] != 0:
                     ret += 1
         for i in range(n):
-            ret += max(grid[i]) << 1
+            ret += max(grid[i])
             t = 0
             for j in range(n):
                 t = max(grid[j][i], t)
-            ret += t << 1
+            ret += t
         return ret
 
 
