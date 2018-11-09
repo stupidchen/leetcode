3d74c469dff90fa49ef2944a879eb3280237fa58
Mike Chen <stupidchen@foxmail.com>
2018-11-09 21:40:37 +0800

    Finished P920.

--- src/P920.py
+++ src/P920.py
@@ -1,3 +1,7 @@
+from functools import lru_cache
+
+MODULO = 10 ** 9 + 7
+
 class Solution:
     def numMusicPlaylists(self, N, L, K):
         """
@@ -6,3 +10,10 @@ class Solution:
         :type K: int
         :rtype: int
         """
+        @lru_cache(None)
+        def dp(i, j):
+            if i == 0:
+                return +(j == 0)
+            ans = dp(i - 1, j - 1) * (N - j + 1) + dp(i - 1, j) * max(j - K, 0)
+            return ans % MODULO
+        return dp(L, N)
