43460d419ea13ad21ee666031467de27a9550db9
Mike Chen <stupidchen@foxmail.com>
2018-12-08 10:30:34 +0800

    Finished P304.

--- src/P304.py
+++ src/P304.py
@@ -4,6 +4,17 @@ class NumMatrix:
         """
         :type matrix: List[List[int]]
         """
+        n = len(matrix)
+        if n == 0:
+            return
+        m = len(matrix[0])
+
+        s = [([0] * (m + 1)) for i in range(n + 1)]
+
+        for i in range(n):
+            for j in range(m):
+                s[i + 1][j + 1] = (s[i + 1][j] + s[i][j + 1] - s[i][j]) + matrix[i][j]
+        self.s = s
 
     def sumRegion(self, row1, col1, row2, col2):
         """
@@ -13,6 +24,12 @@ class NumMatrix:
         :type col2: int
         :rtype: int
         """
+        s = self.s
+        return s[row2 + 1][col2 + 1] - s[row2 + 1][col1] - s[row1][col2 + 1] + s[row1][col1]
+
+
+if __name__ == '__main__':
+    print(NumMatrix([]))
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
