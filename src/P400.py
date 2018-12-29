619d6292a00e1178c5c24db5bb00387198dbf480
Mike Chen <stupidchen@foxmail.com>
2018-12-29 20:48:12 +0800

    Finished P400.

--- src/P400.py
+++ src/P400.py
@@ -4,4 +4,18 @@ class Solution:
         :type n: int
         :rtype: int
         """
-        
+        i = 1
+        tmp = 0
+        while tmp < n:
+            tmp += i * (10 ** i - 10 ** (i - 1))
+            i += 1
+
+        i -= 1
+        tmp -= i * (10 ** i - 10 ** (i - 1))
+        num = (n - tmp - 1) // i + 10 ** (i - 1)
+        index = (n - tmp) - (n - tmp - 1) // i * i
+        return int(str(num)[index - 1])
+
+
+if __name__ == '__main__':
+    print(Solution().findNthDigit(9))
