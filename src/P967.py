d326d8191e4d571e80218a14698b47ad9ba90c91
Mike Chen <stupidchen@foxmail.com>
2019-01-01 21:13:43 +0800

    Finished P967.

---
+++ src/P967.py
@@ -0,0 +1,34 @@
+class Solution:
+    def numsSameConsecDiff(self, N, K):
+        """
+        :type N: int
+        :type K: int
+        :rtype: List[int]
+        """
+        ret = []
+
+        def solve(last, t, n):
+            if n == N:
+                ret.append(''.join(t))
+                return
+
+            if last + K < 10:
+                t[n] = chr(last + K + 48)
+                solve(last + K, t, n + 1)
+
+            if last - K >= 0:
+                t[n] = chr(last - K + 48)
+                solve(last - K, t, n + 1)
+
+        t = [0] * N
+        for i in range(1, 10):
+            t[0] = chr(i + 48)
+            solve(i, t, 1)
+        if N == 1:
+            ret.append('0')
+
+        ret = list(set([int(i) for i in ret]))
+        return ret
+
+if __name__ == '__main__':
+    print(Solution().numsSameConsecDiff(1, 1))
