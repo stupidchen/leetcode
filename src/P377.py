afdf9d48483229d798aa62db40935243af186481
Mike Chen <stupidchen@foxmail.com>
2019-01-11 21:58:16 +0800

    Finished P377.

---
+++ src/P377.py
@@ -0,0 +1,18 @@
+class Solution:
+    def combinationSum4(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        f = [0] * (target + 1)
+        f[0] = 1
+        for i in range(1, target + 1):
+            for num in nums:
+                if i - num >= 0:
+                    f[i] += f[i - num]
+        return f[target]
+
+
+if __name__ == '__main__':
+    print(Solution().combinationSum4([1, 2, 3], 4))
