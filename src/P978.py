61375ea3472f11f435f90c2ec852d29d5afa66bd
Mike Chen <stupidchen@foxmail.com>
2019-01-20 23:48:59 +0800

    Finished P978.

---
+++ src/P978.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxTurbulenceSize(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        n = len(A)
+        f = [1] * n
+        g = [1] * n
+        for i in range(1, n):
+            if A[i] > A[i - 1]:
+                f[i] = g[i - 1] + 1
+            if A[i] < A[i - 1]:
+                g[i] = f[i - 1] + 1
+
+        return max(f + g)
+
+if __name__ == '__main__':
+    print(Solution().maxTurbulenceSize([100]))
