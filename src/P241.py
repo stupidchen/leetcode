ff1a3189b00b5b29fb49504e0455314983480498
Mike Chen <stupidchen@foxmail.com>
2019-02-08 15:02:27 +0800

    Finished P241.

--- src/P241.py
+++ src/P241.py
@@ -1,6 +1,23 @@
 class Solution:
-    def diffWaysToCompute(self, input):
-        """
-        :type input: str
-        :rtype: List[int]
-        """
+    def diffWaysToCompute(self, input: 'str') -> 'List[int]':
+        if input.isdigit():
+            return [int(input)]
+
+        ret = []
+        for i in range(len(input)):
+            if input[i] in '+-*':
+                ret1 = self.diffWaysToCompute(input[:i])
+                ret2 = self.diffWaysToCompute(input[i+1:])
+                for op1 in ret1:
+                    for op2 in ret2:
+                        if input[i] == '+':
+                            ret.append(op1 + op2)
+                        elif input[i] == '-':
+                            ret.append(op1 - op2)
+                        elif input[i] == '*':
+                            ret.append(op1 * op2)
+        return ret
+
+
+if __name__ == '__main__':
+    print(Solution().diffWaysToCompute("2*3-4*5"))
