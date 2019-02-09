8fb84b4350642767f6e86f8a27fc260cc8c5d815
Mike Chen <stupidchen@foxmail.com>
2019-02-09 15:11:30 +0800

    Finished P982.

--- src/P982.py
+++ src/P982.py
@@ -4,19 +4,15 @@ from collections import defaultdict
 class Solution:
     def countTriplets(self, A: 'List[int]') -> 'int':
         d = defaultdict(lambda: 0)
-        n = len(A)
         for a in A:
-            t = 0
-            for i in reversed(range(17)):
-                if a | (1 << i) != a:
-                    t += 1 << i
-            d[t] += 1
-        ret = 0
-        for i in range(n):
-            for j in range(n):
-                t = (1 << 17) - 1 - A[i] & A[j]
-                ret += d[t]
+            for b in A:
+                d[a & b] += 1
 
+        ret = 0
+        for a in A:
+            for b in d.keys():
+                if a & b == 0:
+                    ret += d[b]
 
         return ret
 
