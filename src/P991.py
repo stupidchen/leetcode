032b780e6b082117030ff5115598c82af8ff2294
Mike Chen <stupidchen@foxmail.com>
2019-02-13 11:09:57 +0800

    Finished P991.

---
+++ src/P991.py
@@ -0,0 +1,17 @@
+class Solution:
+    def brokenCalc(self, X: 'int', Y: 'int') -> 'int':
+        x, y = X, Y
+        ret = 0
+        while x < y:
+            ret += 1
+            if y & 1 == 1:
+                y += 1
+            else:
+                y = y >> 1
+        ret += x - y
+
+        return ret
+
+
+if __name__ == '__main__':
+    print(Solution().brokenCalc(1, 1000000000))
