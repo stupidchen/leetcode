7dcbf490e23e7a278db362b6ac8b335841fab391
Mike Chen <stupidchen@foxmail.com>
2019-03-03 21:36:18 +0800

    Finished P376.

--- src/P376.py
+++ src/P376.py
@@ -1,22 +1,22 @@
 class Solution:
     def wiggleMaxLength(self, nums):
         n = len(nums)
-        if n <= 2:
+        if n < 2:
             return n
 
         inc = None
         t = 1
         for i in range(1, n):
-            if nums[i] == nums[i - 1] and inc is None:
-                continue
-            else:
-                if nums[i] < nums[i - 1]:
-                    inc = True
+            if inc is None:
+                if nums[i] == nums[i - 1]:
+                    continue
                 else:
-                    inc = False
+                    if nums[i] < nums[i - 1]:
+                        inc = True
+                    else:
+                        inc = False
 
-
-            if (inc and nums[i] > nums[i - 1]) or (not inc and nums[i] < nums[i - 1]) :
+            if (inc and nums[i] < nums[i - 1]) or (not inc and nums[i] > nums[i - 1]) :
                 t += 1
                 inc = not inc
 
