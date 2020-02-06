7bc57928a72885063e5d0743c55f571a4e1d01f6
Mike Chen <stupidchen@foxmail.com>
2020-02-06 22:34:51 +0800

    Optimized the tester of leetcode solution.

--- src/PTester.py
+++ src/PTester.py
@@ -1,25 +1,21 @@
-import importlib
+class PTester(object):
+    def __init__(self, si, so, cls, tm):
+        if len(si) != len(so):
+            raise ValueError('Number of sample input must equals to the number of sample output!')
 
-problem_no = 'P218'  # Fill problem number here
+        if not hasattr(cls, tm):
+            raise ValueError('Need valid solution!')
 
-if __name__ == '__main__':
-    p = importlib.import_module(problem_no)
-    if not hasattr(p, 'SI') or not hasattr(p, 'SO'):
-        raise ValueError('Need sample input(SI) or sample output(SO)!')
+        self.si, self.so, self.cls, self.tm = si, so, cls, tm
 
-    si, so = p.SI, p.SO
-    if len(si) != len(so):
-        raise ValueError('Number of sample input must equals to the number of sample output!')
-
-    if not hasattr(p, 'Solution') or not hasattr(p, 'TM') or not hasattr(p.Solution, p.TM):
-        raise ValueError('Need valid solution!')
-
-    test_method = p.TM
-    s = p.Solution()
-    for i in range(len(si)):
-        m = getattr(s, test_method)
-        r = m(*si[i])
-        if r != so[i]:
-            print('Test case {} failed'.format(i))
-        else:
-            print('Test case {} passed'.format(i))
+    def run(self):
+        si, so = self.si, self.so
+        s = self.cls()
+        tm = self.tm
+        for i in range(len(si)):
+            m = getattr(s, tm)
+            r = m(*si[i])
+            if r != so[i]:
+                print('Test case {} failed'.format(i))
+            else:
+                print('Test case {} passed'.format(i))
