3494c311d1e2c95f6adca53535084f2c360f3961
Mike Chen <stupidchen@foxmail.com>
2020-02-15 23:15:26 +0800

    Finished P307.

---
+++ src/leetcode/P307.py
@@ -0,0 +1,51 @@
+class FenwickTree(object):
+    def __init__(self, length):
+        self.a = [0] * length
+        self.n = length
+
+    @staticmethod
+    def _last_bit(x):
+        return x ^ (x & (x - 1))
+
+    def update(self, x, d):
+        if x == 0:
+            return
+        t = x
+        n, a = self.n, self.a
+        while t < n:
+            self.a[t] += d
+            t += self._last_bit(t)
+
+    def get(self, x):
+        t, r = x, 0
+        a = self.a
+        while t > 0:
+            r += a[t]
+            t -= self._last_bit(t)
+        return r
+
+
+class NumArray:
+
+    def __init__(self, nums):
+        n = len(nums)
+        self.ta = FenwickTree(n + 1)
+        self.a = nums
+        for i in range(n):
+            self.ta.update(i + 1, nums[i])
+
+    def update(self, i: int, val: int) -> None:
+        self.ta.update(i + 1, val - self.a[i])
+        self.a[i] = val
+
+    def sumRange(self, i: int, j: int) -> int:
+        return self.ta.get(j + 1) - self.ta.get(i)
+
+
+# This kind of input is not supported by tester temporarily
+if __name__ == '__main__':
+    obj = NumArray([1, 3, 5])
+    print(obj.sumRange(0, 2))
+    obj.update(1, 2)
+    print(obj.sumRange(0, 2))
+    print(obj.sumRange(0, 0))
