c1791b024b2be19307299f8b3ccafeafdcfeeea2
Mike Chen <stupidchen@foxmail.com>
2020-06-11 21:04:56 +0800

    Finished P1461(Biweekly Contest 27).

---
+++ src/leetcode/P1461.py
@@ -0,0 +1,23 @@
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        n = len(s)
+        d = set()
+        for i in range(k):
+            for j in range(n - i):
+                d.add(s[j: j + i + 1])
+
+        for i in range(1 << (k + 1) - 1):
+            f = ''
+            t = i
+            while t > 0:
+                f = chr(48 + (t & 1)) + f
+                t = t >> 1
+
+            f = '0' * (k - len(f)) + f
+            if f not in d:
+                return False
+        return True
+
+
+if __name__ == '__main__':
+    print(Solution().hasAllCodes(s="0000000001011100", k=4))
