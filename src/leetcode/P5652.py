a7eef3ff751a5e6c2c4f8380fc164458b26a775b
Mike Chen <stupidchen@foxmail.com>
2021-01-10 11:31:59 +0800

    Finished P5652(P1721, Weekly Contest 223).

---
+++ src/leetcode/P5652.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def swapNodes(self, head: ListNode, k: int):
+        p = ListNode(0, head)
+        a = []
+        t = p
+        while t is not None:
+            a.append(t)
+            t = t.next
+
+        q = a[-k]
+        a[-k] = a[k]
+        a[k] = q
+
+        for i in range(len(a) - 1):
+            a[i].next = a[i + 1]
+        a[-1].next = None
+
+        return p.next
+
+if __name__ == '__main__':
+    r = ListNode(1, ListNode(2))
+    r = Solution().swapNodes(r, 1)
