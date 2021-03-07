6ca4486bfcd867af04be88ae97c00e9c5ddab791
Mike Chen <stupidchen@foxmail.com>
2021-03-07 18:11:39 +0800

    Finished P3663(P706).

---
+++ src/leetcode/P3663.py
@@ -0,0 +1,34 @@
+M = 10 ** 6 + 1
+
+
+class MyHashMap:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.map = [-1] * M
+
+    def put(self, key: int, value: int) -> None:
+        """
+        value will always be non-negative.
+        """
+        self.map[key] = value
+
+    def get(self, key: int) -> int:
+        """
+        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+        """
+        return self.map[key]
+
+    def remove(self, key: int) -> None:
+        """
+        Removes the mapping of the specified value key if this map contains a mapping for the key
+        """
+        self.map[key] = -1
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
