a250924f59ff2520cefccd23c8bc008afaa4cbdc
stupidchen <stupidchen@foxmail.com>
2021-09-07 22:28:39 +0800

    Finished P3966(P206).

--- README.md
+++ README.md
@@ -955,3 +955,4 @@ This repository includes the **original** solution of algorithm problems from th
 |1|4|3963|834|
 |1|5|3964|899|
 |1|6|3965|1629|
+|1|7|3966|206|
---
+++ src/leetcode/goSolution/P3966.go
@@ -0,0 +1,11 @@
+func reverseList(head *ListNode) *ListNode {
+	var tmp *ListNode = nil
+	var last *ListNode = nil
+	for i := head; i != nil; i = tmp {
+		tmp = i.Next
+		i.Next = last
+		last = i
+	}
+
+	return last
+}
