0ee4d0cd281e6927d545aadd0bd87215fc06262d
stupidchen <stupidchen@foxmail.com>
2021-10-31 15:22:10 +0800

    Finished P5914(P2057, Weekly Contest 265).

---
+++ src/leetcode/P5914.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def smallestEqual(self, nums: List[int]) -> int:
+        for i, v in enumerate(nums):
+            if i % 10 == v:
+                return i
+        return -1
