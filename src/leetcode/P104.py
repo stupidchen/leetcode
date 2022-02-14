7ead86f638d44a0ec0908ccbb574858a039ef18f
stupidchen <stupidchen@foxmail.com>
2022-02-14 21:18:13 +0800

    Finished P104.

---
+++ src/leetcode/P104.py
@@ -0,0 +1,8 @@
+from typing import Optional
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
